ac4ed7b7ffb77910cca0bd174d5997f5951a8dd7
JaraOps <iggyjara@gmail.com>
2025-10-04 15:19:03 -0700

    main python files and data

--- src/streamlit_AstroSight.py
+++ src/streamlit_AstroSight.py
@@ -1,1 +1,1 @@
-#create web aplication with streamlit including knowledge graph, add AI for purpose driven summaries
+#create web application with streamlit including knowledge graph, add AI for purpose driven summaries
