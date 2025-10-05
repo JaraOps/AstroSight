590b55518f425e2e4b91b3f6451314f2fe3a44c8
JaraOps <iggyjara@gmail.com>
2025-10-05 04:19:05 -0700

    modifications

--- src/setup.sh
+++ src/setup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.8.0/en_core_web_sm-3.8.0.tar.gz
-python -m spacy link en_core_web_sm en_core_web_sm --force
+
+# 1. Create a dedicated, guaranteed data directory for spaCy
+mkdir -p .spacy_data
+
+# 2. Set environment variable to force spaCy to look in this directory
+export SPACY_DATA=./.spacy_data
+
+# 3. Download and install the model package directly into the custom directory
+# --no-deps ensures only the model package itself is installed here
+pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.8.0/en_core_web_sm-3.8.0.tar.gz -t .spacy_data --no-deps
+
+# 4. Create the symbolic link inside the custom directory
+# This resolves the E050 "Can't find model" error
+python -m spacy link .spacy_data/en_core_web_sm-3.8.0 en_core_web_sm --force
