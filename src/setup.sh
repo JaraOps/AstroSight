0a4e32664699bb688812fe5c750177dee5f5a2ae
JaraOps <iggyjara@gmail.com>
2025-10-05 04:09:58 -0700

    modifications

--- src/setup.sh
+++ src/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Download and install the model package directly
+# 1. Download and install the model package directly
+# This ensures the model files are present
 pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.8.0/en_core_web_sm-3.8.0.tar.gz
 
-# Link the installed package to the name 'en_core_web_sm'
-python -m spacy link en_core_web_sm en_core_web_sm --forcece
+# 2. Link the installed package to the name 'en_core_web_sm'
+# This resolves the [E050] "Can't find model" error
+python -m spacy link en_core_web_sm en_core_web_sm --force
